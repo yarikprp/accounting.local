a09e816543527aabd9c9e09e8d52c2275ff30094
Yaroslav <121134813+yarikprp@users.noreply.github.com>
2025-02-17 16:07:41 +0300

    Добавил шаблоны страниц

--- src/router/index.js
+++ src/router/index.js
@@ -1,11 +1,59 @@
 import { createRouter, createWebHistory } from "vue-router";
-import HomeView from "../views/HomeView.vue";
 
 const routes = [
   {
+    path: "/login",
+    name: "login",
+    meta: { layout: "empty" },
+    component: () => import("../views/Login.vue"),
+  },
+  {
+    path: "/register",
+    name: "register",
+    meta: { layout: "empty" },
+    component: () => import("../views/Register.vue"),
+  },
+  {
     path: "/",
     name: "home",
-    component: HomeView,
+    meta: { layout: "main" },
+    component: () => import("../views/Home.vue"),
+  },
+  {
+    path: "/categories",
+    name: "categories",
+    meta: { layout: "main" },
+    component: () => import("../views/Categories.vue"),
+  },
+  {
+    path: "/detail/:id",
+    name: "detail",
+    meta: { layout: "main" },
+    component: () => import("../views/Detail.vue"),
+  },
+  {
+    path: "/history",
+    name: "history",
+    meta: { layout: "main" },
+    component: () => import("../views/History.vue"),
+  },
+  {
+    path: "/planning",
+    name: "planning",
+    meta: { layout: "main" },
+    component: () => import("../views/Planning.vue"),
+  },
+  {
+    path: "/profile",
+    name: "profile",
+    meta: { layout: "main" },
+    component: () => import("../views/Profile.vue"),
+  },
+  {
+    path: "/record",
+    name: "record",
+    meta: { layout: "main" },
+    component: () => import("../views/Record.vue"),
   },
 ];
 
