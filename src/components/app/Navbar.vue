5b163a37f1bc84705f1c2c2da9e281761cac93b1
Yaroslav <121134813+yarikprp@users.noreply.github.com>
2025-02-17 20:14:16 +0300

    сделал dropdawn, вывел дату с временем

--- src/components/app/Navbar.vue
+++ src/components/app/Navbar.vue
@@ -5,7 +5,7 @@
         <a href="#" @click.prevent="toggleSidebar">
           <i class="material-icons black-text">dehaze</i>
         </a>
-        <span class="black-text">12.12.12</span>
+        <span class="black-text">{{ formatDate(date, "datetime") }}</span>
       </div>
 
       <ul class="right hide-on-small-and-down">
@@ -14,6 +14,7 @@
             class="dropdown-trigger black-text"
             href="#"
             data-target="dropdown"
+            ref="dropdown"
           >
             USER NAME
             <i class="material-icons right">arrow_drop_down</i>
@@ -21,13 +22,13 @@
 
           <ul id="dropdown" class="dropdown-content">
             <li>
-              <a href="#" class="black-text">
+              <router-link to="/profile" class="black-text">
                 <i class="material-icons">account_circle</i>Профиль
-              </a>
+              </router-link>
             </li>
             <li class="divider" tabindex="-1"></li>
             <li>
-              <a href="#" class="black-text">
+              <a href="#" class="black-text" @click.prevent="logout">
                 <i class="material-icons">assignment_return</i>Выйти
               </a>
             </li>
@@ -39,18 +40,43 @@
 </template>
 
 <script>
+import M from "materialize-css";
+import "materialize-css/dist/css/materialize.min.css";
+import formatDate from "@/filters/formatDate";
+
 export default {
   name: "navBar",
   data() {
     return {
       isSidebarOpen: false,
+      date: new Date(),
+      interval: null,
+      dropdown: null,
     };
   },
+  mounted() {
+    this.interval = setInterval(() => {
+      this.date = new Date();
+    }, 1000);
+    this.dropdown = M.Dropdown.init(this.$refs.dropdown, {
+      constrainWidth: false,
+    });
+  },
+  beforeUnmount() {
+    clearInterval(this.interval);
+    if (this.dropdown && this.dropdown.destroy) {
+      this.dropdown.destroy();
+    }
+  },
   methods: {
+    logout() {
+      this.$router.push("/login?message=logout");
+    },
     toggleSidebar() {
       this.isSidebarOpen = !this.isSidebarOpen;
       this.$emit("toggle-sidebar", this.isSidebarOpen);
     },
+    formatDate,
   },
 };
 </script>
--- src/main.js
+++ src/main.js
@@ -4,5 +4,12 @@ import "./registerServiceWorker";
 import router from "./router";
 import store from "./store";
 import "materialize-css/dist/js/materialize.min";
+import dateFilter from "./filters/formatDate";
+
+const app = createApp(App);
+
+app.config.globalProperties.$filters = {
+  date: dateFilter,
+};
 
 createApp(App).use(store).use(router).mount("#app");
