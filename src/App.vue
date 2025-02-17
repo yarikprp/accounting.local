0171206d6777c84f0c876920651956548de605ba
Yaroslav <121134813+yarikprp@users.noreply.github.com>
2025-02-17 14:42:39 +0300

    Добавил библиотеку и index.css

--- src/App.vue
+++ src/App.vue
@@ -1,4 +1,10 @@
 <template>
-  <div></div>
-  <router-view />
+  <div id="app">
+    <router-view />
+  </div>
 </template>
+
+<style lang="scss">
+@import "~materialize-css/dist/css/materialize.min.css";
+@import "assets/index.css";
+</style>
