e693053bc3dc00cae0c82660dab268108446a603
Yaroslav <121134813+yarikprp@users.noreply.github.com>
2025-02-27 16:59:58 +0300

    Установил и настроил firebase

--- src/main.js
+++ src/main.js
@@ -5,20 +5,39 @@ import router from "./router";
 import store from "./store";
 import "materialize-css/dist/js/materialize.min";
 import dateFilter from "./filters/formatDate";
-import vuelidate from "vuelidate/lib/validators";
 import Toast, { POSITION } from "vue-toastification";
 import "vue-toastification/dist/index.css";
+import { initializeApp } from "firebase/app";
+import { getAuth, onAuthStateChanged } from "firebase/auth";
 
-const app = createApp(App);
-
-app.use(Toast, {
-  position: POSITION.TOP_RIGHT,
-  timeout: 3000,
-  draggable: true,
-});
-
-app.config.globalProperties.$filters = {
-  date: dateFilter,
+const firebaseConfig = {
+  apiKey: "AIzaSyBnvgwPbZMsqgchtotB2C-0Tiv8e98n43E",
+  authDomain: "accunting-crm.firebaseapp.com",
+  projectId: "accunting-crm",
+  storageBucket: "accunting-crm.firebasestorage.app",
+  messagingSenderId: "1024550973322",
+  appId: "1:1024550973322:web:9687ae763f33bab9a86273",
+  measurementId: "G-WWW2VNC406",
 };
 
-createApp(App).use(store).use(router).use(vuelidate).mount("#app");
+const firebaseApp = initializeApp(firebaseConfig);
+
+const auth = getAuth(firebaseApp);
+
+onAuthStateChanged(auth, () => {
+  const app = createApp(App);
+
+  app.use(store);
+  app.use(router);
+  app.use(Toast, {
+    position: POSITION.TOP_RIGHT,
+    timeout: 3000,
+    draggable: true,
+  });
+
+  app.config.globalProperties.$filters = {
+    date: dateFilter,
+  };
+
+  app.mount("#app");
+});
